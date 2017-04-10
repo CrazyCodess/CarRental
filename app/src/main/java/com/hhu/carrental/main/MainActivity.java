db1be9fb5de82e249b4ed742aefdfb3fe758dbe8
CrazyCodess <1312480794@qq.com>
2017-04-10 13:43:37 +0800

    location

--- app/src/main/java/com/hhu/carrental/main/MainActivity.java
+++ app/src/main/java/com/hhu/carrental/main/MainActivity.java
@@ -2,86 +2,206 @@ package com.hhu.carrental.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.BitmapDescriptorFactory;
-import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapBaseIndoorMapInfo;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.Marker;
-import com.baidu.mapapi.map.MarkerOptions;
-import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 import com.hhu.carrental.R;
-
-import static com.baidu.location.h.g.R;
+import com.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends Activity {
 
     MapView mapView = null;
+    private LocationMode mLocMode;
     //public LocationClient mLocationClient = null;
-    //public BDLocationListener myListenter = new MyLocationListener();
+    public BDLocationListener myListenter = new MyLocationListenner();
+    MapBaseIndoorMapInfo mMapBaseIndoorMapInfo = null;
     BaiduMap baiduMap=null;
     Marker marker = null;
+    ImageButton slidebtn = null;
+    ImageButton locbtn = null;
+    public LocationClient mLocationClient = null;
+    boolean isFirstLoc = true;
+    private BitmapDescriptor mCurrentMarker;
+    private SlidingMenu slidMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
-        //注意该方法要再setContentView方法之前实现
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SDKInitializer.initialize(getApplicationContext());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        SDKInitializer.initialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
-        mapView=(MapView)findViewById(R.id.bmapView);
-        baiduMap = mapView.getMap();
-        baiduMap.setMyLocationEnabled(true);
+        initmap();
+        location();
 
+    }
 
-        //定义Maker坐标点
-        LatLng point = new LatLng(39.963175, 116.400244);
-//构建Marker图标
-        BitmapDescriptor bitmap = BitmapDescriptorFactory
-                .fromResource(R.drawable.huaji);
-//构建MarkerOption，用于在地图上添加Marker
-        OverlayOptions option = new MarkerOptions()
-                .position(point)
-                .icon(bitmap);
-//在地图上添加Marker，并显示
-        marker = (Marker)baiduMap.addOverlay(option);
-
-        baiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+    private void location(){
+        locbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMarkerClick(Marker marker) {
-                Button button = new Button(getApplicationContext());
-                button.setBackgroundResource(R.drawable.hint);
-                LatLng location = new LatLng(39.963175,116.400244);
-                InfoWindow minfowindow = new InfoWindow(button ,location,-47);
-                baiduMap.showInfoWindow(minfowindow);
-                return true;
+            public void onClick(View v) {
+                switch (mLocMode){
+                    case NORMAL://处于正常模式
+                        mLocMode = LocationMode.FOLLOWING;
+                        locbtn.setImageResource(R.drawable.location0);
+                        baiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                                mLocMode,true,mCurrentMarker
+                        ));
+                        break;
+                    case COMPASS://处于罗盘模式
+                        mLocMode = LocationMode.NORMAL;
+                        locbtn.setImageResource(R.drawable.location0);
+                        baiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                                mLocMode,true,mCurrentMarker
+                        ));
+                        break;
+                    case FOLLOWING://处于跟随模式
+                        mLocMode = LocationMode.COMPASS;
+                        locbtn.setImageResource(R.drawable.location1);
+                        baiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                                mLocMode,true,mCurrentMarker
+                        ));
+                        break;
+                    default:break;
+                }
+
             }
         });
+        mLocationClient.start();
 
-
-        //定位
-        //mLocationClient = new LocationClient(getApplicationContext());
-       // mLocationClient.registerLocationListener(myListenter);
     }
+
+
+    private void initmap(){
+        locbtn = (ImageButton)findViewById(R.id.loc_btn);
+        mapView=(MapView)findViewById(R.id.bmapView);
+        baiduMap = mapView.getMap();
+        baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        baiduMap.setTrafficEnabled(false);
+        baiduMap.setIndoorEnable(true);
+        baiduMap.setMyLocationEnabled(true);
+        baiduMap.setBuildingsEnabled(true);
+        baiduMap.setMaxAndMinZoomLevel(3,21);
+        mLocMode = LocationMode.NORMAL;
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(myListenter);
+        mLocMode = LocationMode.NORMAL;
+        initSlidingMenu();
+        initLocation();
+
+    }
+    private void initLocation(){
+        //配置定位SDK各配置参数
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setLocationNotify(true);
+        option.setIsNeedLocationDescribe(true);
+        option.setIsNeedLocationPoiList(true);
+        option.setIgnoreKillProcess(false);
+        option.SetIgnoreCacheException(false);
+        option.setEnableSimulateGps(false);
+        mLocationClient.setLocOption(option);
+
+    }
+
+    public class MyLocationListenner implements BDLocationListener {
+
+        private String lastFloor = null;
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if(location == null || mapView==null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            baiduMap.setMyLocationData(locData);
+            if(isFirstLoc){
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                baiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+
+
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
+
+        public void onConnectHotSpotMessage(String s, int i){
+
+        }
+    }
+
+    private void initSlidingMenu(){
+        slidMenu = new SlidingMenu(this);
+
+        slidMenu.setMode(SlidingMenu.LEFT);
+        slidMenu.setSecondaryMenu(R.layout.slide_layout);
+        slidMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidMenu.setShadowWidth(10);
+        slidMenu.setBehindOffsetRes(R.dimen.sliding_menu_offset);// 设置偏离距离
+        slidMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 全屏模式，全屏滑动都可打开
+        slide();
+    }
+
+    /**
+     * 侧边栏操作
+     */
+    private void slide(){
+        slidebtn = (ImageButton)findViewById(R.id.slide_btn);
+        slidebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                slidMenu.toggle();
+            }
+        });
+    }
+
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mLocationClient.stop();
+        baiduMap.setMyLocationEnabled(false);
+        mapView = null;
         mapView.onDestroy();
+        super.onDestroy();
     }
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+
         mapView.onResume();
     }
     @Override
@@ -89,29 +209,6 @@ public class MainActivity extends Activity {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mapView.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-/*        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);*/
-        return true;
     }
 
 
